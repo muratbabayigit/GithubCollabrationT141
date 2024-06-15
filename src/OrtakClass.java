f0fb0fde6a2a8fa2b82baaca9191b2dc6cabab32
bulutluoz <abbulutluoz@gmail.com>
2024-06-15 17:31:58 +0200

    ahmet ortak class degisiklik

--- src/OrtakClass.java
+++ src/OrtakClass.java
@@ -1,7 +1,7 @@
 public class OrtakClass {
 
     public static void main(String[] args) {
-
+        System.out.println("Java Candir");
         System.out.println("Buradan babayigit geçti");
     }
 }
